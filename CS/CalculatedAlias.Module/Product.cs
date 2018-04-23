3ae47f86eab45c180e3c9bf9e439c78a58b4dad0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CalculatedAlias.Module/Product.cs
@@ -0,0 +1,51 @@
+using System;
+using System.ComponentModel;
+
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+
+using DevExpress.ExpressApp;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+
+namespace CalculatedAlias.Module {
+    [CalculatedPersistentAlias("ProductName", "ProductNameAlias"), DefaultClassOptions]
+    public class Product : BaseObject {
+        public Product(Session session) : base(session) { }
+
+        private static string productNameFormat = "{Manufacturer} {Model}";
+        private static string productNameAlias = "concat(Manufacturer, Model)";
+        
+        public string ProductName {
+            get { return ObjectFormatter.Format(productNameFormat, this, EmptyEntriesMode.RemoveDelimeterWhenEntryIsEmpty); }
+        }
+        [Browsable(false)]
+        public string ProductNameAlias {
+            get { return productNameAlias; }            
+        }
+
+        public static void SetProductNameFormat(string productNameFormat, string productNameAlias) {
+            if(!string.IsNullOrEmpty(productNameFormat)) {
+                Product.productNameFormat = productNameFormat;
+            }
+            if(!string.IsNullOrEmpty(productNameAlias)) {
+                Product.productNameAlias = productNameAlias;
+            }
+        }
+
+        public string Manufacturer {
+            get { return GetPropertyValue<string>("Manufacturer"); }
+            set { SetPropertyValue<string>("Manufacturer", value); }
+        }
+        public string Model {
+            get { return GetPropertyValue<string>("Model"); }
+            set { SetPropertyValue<string>("Model", value); }
+        }
+        public string Revision {
+            get { return GetPropertyValue<string>("Revision"); }
+            set { SetPropertyValue<string>("Revision", value); }
+        }
+    }
+
+}
---
+++ VB/CalculatedAlias.Module/Module.vb
@@ -0,0 +1,19 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+
+Imports DevExpress.ExpressApp
+Imports System.Reflection
+Imports System.Configuration
+
+
+Namespace CalculatedAlias.Module
+	Public NotInheritable Partial Class CalculatedAliasModule
+		Inherits ModuleBase
+		Public Sub New()
+			InitializeComponent()
+
+			Product.SetProductNameFormat(ConfigurationManager.AppSettings("ProductNameFormat"), ConfigurationManager.AppSettings("ProductNameAlias"))
+		End Sub
+	End Class
+End Namespace
