85092f7d1777c529c6c26495eb8963b75e68cc8b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CalculatedAlias.Module/Updater.cs
@@ -0,0 +1,34 @@
+using System;
+
+using DevExpress.ExpressApp.Updating;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.Persistent.BaseImpl;
+
+namespace CalculatedAlias.Module {
+    public class Updater : ModuleUpdater {
+        public Updater(IObjectSpace objectSpace, Version currentDBVersion) : base(objectSpace, currentDBVersion) { }
+        public override void UpdateDatabaseAfterUpdateSchema() {
+            base.UpdateDatabaseAfterUpdateSchema();
+
+            Product productOne = ObjectSpace.CreateObject<Product>();
+            productOne.Manufacturer = "Company A";
+            productOne.Model = "Model One";
+            productOne.Revision = "Revision One";
+            productOne.Save();
+
+            Product productTwo = ObjectSpace.CreateObject<Product>();
+            productTwo.Manufacturer = "Company B";
+            productTwo.Model = "Model Two";
+            productTwo.Revision = "Revision Two";
+            productTwo.Save();
+          
+            Product productThree = ObjectSpace.CreateObject<Product>();
+            productThree.Manufacturer = "Company C";
+            productThree.Model = "Model Three";
+            productThree.Revision = "Revision Three";
+            productThree.Save();     
+        }
+    }
+}
