1aab74ce2edb25f3eeb0a9d4f34fe575a0f15b34
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CalculatedAlias.Web/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Language="vb" CodeBehind="Global.asax.vb" Inherits="CalculatedAlias.Web.Global"%>
---
+++ VB/CalculatedAlias.Win/WinApplication.vb
@@ -0,0 +1,21 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.Win
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.ExpressApp
+
+Namespace CalculatedAlias.Win
+	Partial Public Class CalculatedAliasWindowsFormsApplication
+		Inherits WinApplication
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub CalculatedAliasWindowsFormsApplication_DatabaseVersionMismatch(ByVal sender As Object, ByVal e As DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs) Handles MyBase.DatabaseVersionMismatch
+			e.Updater.Update()
+			e.Handled = True
+		End Sub
+	End Class
+End Namespace
